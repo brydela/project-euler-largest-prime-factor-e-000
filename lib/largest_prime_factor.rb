6078d227bb9cc60f61a0ee9679cccba8253f70b4
brydela <akestrelgrey@gmail.com>
2015-11-07 23:55:20 +0000

    nitrous-backup at Sat Nov  7 23:55:19 UTC 2015

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+#PROBLEM: LARGEST PRIME FACTOR
+
+#The prime factors of 13195 are 5, 7, 13 and 29. The largest prime factor in this case is 29.
+#What is the largest prime factor of the number 600851475143
+
+def largest_prime_factor(input)
+  factors = []
+  (1..input).each do |x|
+    if input % x == 0
+      factors << x
+      x += 1
+    else
+      x == input
+      break
+    end
+  end
+end
